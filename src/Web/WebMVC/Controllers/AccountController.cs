fbfac6fb4970b131d2d5e649a7bed43d40fbde80
Pedro <pedrodaluzmachado@gmail.com>
2022-06-21 13:37:32 -0300

    Controllers

---
+++ src/Web/WebMVC/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EShoppMarketPlace.src.Web.WebMVC
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+       // public async Task<IActionResult> SignIn(string returnUrl)
+      //  {
+       //     var user = User as ClaimsPrincipal;
+            //var token = await HttpContent.GetTokenAsync("access_token");
+      //  }
+    }
+}
---
+++ src/Web/WebMVC/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShoppMarketPlace.src.Web.WebMVC.Controllers
+{
+    [Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+    public class CartController : Controller
+    {
+       // private readonly ICart
+    }
+}
